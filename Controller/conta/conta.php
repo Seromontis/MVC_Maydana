33ae11ead9b69e93c8d61c564b4aa01042b1203c
Matheus Maydana <matheus@objetivawebsites.com.br>
2018-04-29 22:57:09 -0300

    29/04/2018 - Consultas DB sem SQL
    faze de testes... - EM DESENVOLVIMENTO -

--- Controller/conta/conta.php
+++ Controller/conta/conta.php
@@ -4,8 +4,8 @@
 		"AUTHOR":"Matheus Maydana",
 		"CREATED_DATA": "26/04/2018",
 		"CONTROLADOR": "Index",
-		"LAST EDIT": "26/04/2018",
-		"VERSION":"0.0.1"
+		"LAST EDIT": "29/04/2018",
+		"VERSION":"0.0.2"
 	}
 */
 class Conta {
@@ -23,9 +23,74 @@ class Conta {
 		** @param = nome bigode de gato {{exemplo}} - ARRAY ou STRING
 		**/
 		$GOD = new Model_GOD;
+		$dados = array();
 
-		//new de($GOD->getPessoa(2));
+		/*
+		// UPDATE
+		$dados['tabela'] = 'conta';
+		$dados['acao'] = 'update';
+		$dados['valor']  = array(
+			'email' 		=> '123'
+		);
+		$dados['where']  = array(
+			'email' 	 => 'mattheuszcabal@gmail.com',
+			'senha' => 'asdg1124'
+		);
+		$dados['param'] = array(
+			'emailNew'	=> $dados['valor']['email'],
+			'email' 	=> $dados['where']['email'],
+			'senha'		=> $dados['where']['senha']
+		);*/
 
+		/**/
+		// INSERT
+		/*$dados['tabela'] = 'conta';
+		$dados['acao'] = 'insert';
+		$dados['valor'] = array(
+			'email' 		=> 'email@test.com',
+			'senha' 		=> '1234',
+			'data_criacao' 	=> $GOD->_hoje(),
+			'hora_criacao' 	=> $GOD->_agora(),
+		);
+		$dados['param'] = array(
+			'email' 		=> $dados['valor']['email'],
+			'senha' 		=> $dados['valor']['senha'],
+			'data_criacao' 	=> $dados['valor']['data_criacao'],
+			'hora_criacao' 	=> $dados['valor']['hora_criacao'],
+		);*/
+
+		// DELETE
+		/*$dados['tabela'] = 'conta';
+		$dados['acao'] = 'delete';
+		$dados['valor'] = array(
+			'email' => 'novoemail@test.com',
+			'senha' => '99999'
+		);
+		$dados['param'] = array(
+			'email' => $dados['valor']['email'],
+			'senha' => $dados['valor']['senha'],
+		);*/
+
+
+		$dados['tabela'] = 'conta';
+		$dados['acao'] = 'select';
+		$dados['valor'] = array(
+			'email' => 'novoemail@test.com',
+			'senha' => '99999'
+		);
+		$dados['select'] = array(
+			'email',
+			'senha',
+			'data_criacao'
+		);
+		$dados['param'] = array(
+			'email' => $dados['valor']['email'],
+			'senha' => $dados['valor']['senha'],
+		);
+
+		$gerarSQL = $GOD->SQL($dados);
+
+		new de($gerarSQL);
 		$mustache = array(
 			'{{header}}' => $GOD->headerHTML()
 		);
--- Model/GOD.php
+++ Model/GOD.php
@@ -4,8 +4,8 @@
 	"AUTHOR":"Matheus Mayana",
 	"CREATED_DATA": "09/04/2018",
 	"MODEL": "GOD",
-	"LAST EDIT": "09/04/2018",
-	"VERSION":"0.0.1"
+	"LAST EDIT": "29/04/2018",
+	"VERSION":"0.0.2"
 }
 */
 class Model_God extends Model_Functions_Functions{
@@ -14,4 +14,101 @@ class Model_God extends Model_Functions_Functions{
 	function __construct(){
 	}
 
+	function gerarSQL($dados){
+
+		$sql = '';
+		if(is_array($dados) and !empty($dados) and count($dados) > 0 and isset($dados['tabela']) and !empty($dados['tabela'])){
+
+			if($dados['acao'] == 'update'){
+
+				$sql = '';
+				$tabela = $dados['tabela'];
+				$valor  = $dados['valor'];
+				$valor = '';
+
+				$forBind = array();
+				foreach($dados['valor'] as $key => $value){
+					$key = $this->basico($key);
+					$value = $this->basico($value);
+					$valor .= $key.' = :'.$key.'New'.', ';
+					$forBind[$key] = $value;
+				}
+
+				$where = '';
+				foreach($dados['where'] as $key => $value){
+					$key = $this->basico($key);
+					$value = $this->basico($value);
+					$where .= $key.' = :'.$key.' AND ';
+					$forBind[$key] = $value;
+				}
+
+				$valor = trim($valor, ', ');
+				$where = trim($where, ' AND ');
+
+				$sql = 'UPDATE '.$tabela.' SET '.$valor.' WHERE '.$where;
+
+				return $sql;
+
+			}elseif($dados['acao'] == 'insert'){
+
+				$sql = '';
+				$tabela = $dados['tabela'];
+				$keys = '';
+				$values = '';
+
+				$forBind = array();
+				foreach($dados['valor'] as $key => $value){
+					$key = $this->basico($key);
+					$keys .= $key.', ';
+					$values .= ':'.$key.', ';
+					$forBind[$key] = $value;
+				}
+
+				$keys = trim($keys, ', ');
+				$values = trim($values, ', ');
+
+				$sql = 'INSERT INTO '.$tabela.'('.$keys.') VALUES ('.$values.')';
+
+				return $sql;
+
+			}elseif($dados['acao'] == 'delete'){
+
+				$sql = '';
+				$tabela = $dados['tabela'];
+				$values = '';
+				foreach($dados['valor'] as $key => $value){
+					$key = $this->basico($key);
+					$values .= $key.' = :'.$key.' AND ';
+				}
+
+				$values = trim($values, ' AND ');
+
+				$sql = 'DELETE FROM '.$tabela.' WHERE '.$values;
+				return $sql;
+
+			}elseif($dados['acao'] == 'select'){
+
+				$sql = '';
+				$tabela = $dados['tabela'];
+				$select = '';
+				$where = '';
+				foreach($dados['select'] as $key => $value){
+					$value = $this->basico($value);
+					$select .= $value.', ';
+				}
+
+				foreach($dados['param'] as $key => $value){
+					$value = $this->basico($key);
+					$where .= $key.' = :'.$key.' AND ';
+				}
+
+				$select = trim($select, ', ');
+				$where = trim($where, ' AND ');
+
+				$sql = 'SELECT '.$select.' FROM '.$tabela.' WHERE '.$where;
+				return $sql;
+			}
+		}
+
+	}
 }
