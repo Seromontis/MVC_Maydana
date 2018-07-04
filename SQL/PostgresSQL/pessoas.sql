a11c16bf642a124bcc1f4bd325a177e8a5a90ae6
Matheus Maydana <matheus@objetivawebsites.com.br>
2018-07-04 18:15:15 -0300

    04/07/2018 - SQL - Pessoas
    -

--- SQL/PostgresSQL/pessoas.sql
+++ SQL/PostgresSQL/pessoas.sql
@@ -4,19 +4,21 @@
 
 CREATE TABLE pessoas
 (
+  id serial NOT NULL,
   id_conta integer,
+  tipo smallint DEFAULT 1, -- 1 = usuario...
   nome character varying(150), -- nome completo do cidadão
+  cid_codigo integer, -- codigo da cidade
+  est_codigo integer, -- codigo do estado
+  bai_codigo integer, -- codigo do bairro
   sexo smallint, -- 1 = masculino...
   nascimento character varying(30),
   cpf integer,
   rg integer,
-  cid_codigo integer, -- codigo da cidade
-  est_codigo integer, -- codigo do estado
-  bai_codigo integer, -- codigo do bairro
   whatsapp character varying(30),
   telefone character varying(30),
   celular character varying(30),
-  id serial NOT NULL,
+  descricao character varying(250),
   CONSTRAINT pessoas_pkey PRIMARY KEY (id),
   CONSTRAINT pessoas_id_conta_fkey FOREIGN KEY (id_conta)
       REFERENCES conta (id_conta) MATCH SIMPLE
@@ -34,4 +36,9 @@ COMMENT ON COLUMN pessoas.sexo IS '1 = masculino
 COMMENT ON COLUMN pessoas.cid_codigo IS 'codigo da cidade';
 COMMENT ON COLUMN pessoas.est_codigo IS 'codigo do estado';
 COMMENT ON COLUMN pessoas.bai_codigo IS 'codigo do bairro';
+COMMENT ON COLUMN pessoas.tipo IS '1 = usuario
+2 = cliente
+3 = funcionario
+4 = gerente
+5 = GOD';
 
